97a46c1c345c57844904fa425d36a046abb0e703
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2020-12-17 16:00:43 +0530

    Setup app for deployment in heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode  = require("./utilis/geocode");
 const forecast = require("./utilis/forecast");
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -96,6 +97,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server on port 3000:')
+app.listen(port, () => {
+    console.log('Server on port '+port+':')
 })
